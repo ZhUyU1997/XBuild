79771814c2f5b5e036e3d4b3d1225a654b789f26
yzhu <891085309@qq.com>
2020-07-07 23:03:17 +0800

    修复子目录递归的目录错误

--- scripts/rule.mk
+++ scripts/rule.mk
@@ -3,7 +3,7 @@ include $(XBUILD_DIR)/include.mk
 quiet_cmd_cc_o_c = $(ECHO_CC) $(@:.o=)
 cmd_cc_o_c = $(CC) $(X_CFLAGS) -MD -MF $(@D)/.$(@F).d $(X_CPPFLAGS) -c $< -o $@
 
-quiet_cmd_as_o_S = $(ECHO_CC) $(@:.o=)
+quiet_cmd_as_o_S = $(ECHO_AS) $(@:.o=)
 cmd_as_o_S = $(AS) $(X_ASFLAGS) -MD -MF $(@D)/.$(@F).d $(X_CPPFLAGS) -c $< -o $@
 
 # If the list of objects to link is empty, just create an empty built-in.o
